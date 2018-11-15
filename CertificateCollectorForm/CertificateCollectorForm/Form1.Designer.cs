0f61775e2c8af647699e92b9a6c7f35a6e530282
Dávid Kalácska <david.kalacska@dealogic.com>
2018-11-15 13:21:19 +0100

    fix UI

--- CertificateCollectorForm/CertificateCollectorForm/Form1.Designer.cs
+++ CertificateCollectorForm/CertificateCollectorForm/Form1.Designer.cs
@@ -28,70 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(134, 12);
+            this.textBox1.Location = new System.Drawing.Point(108, 6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(421, 20);
+            this.textBox1.Size = new System.Drawing.Size(697, 20);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(561, 9);
+            this.button1.Location = new System.Drawing.Point(900, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Analyze";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 144);
+            this.listBox1.Location = new System.Drawing.Point(15, 65);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(617, 316);
+            this.listBox1.Size = new System.Drawing.Size(960, 264);
             this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Folder to analyze:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(173, 118);
+            this.textBox2.Location = new System.Drawing.Point(117, 363);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.textBox2.Size = new System.Drawing.Size(688, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Export result to file:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(900, 361);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(811, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Browse..";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(811, 361);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Browse..";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 36);
+            this.progressBar1.MarqueeAnimationSpeed = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(960, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(641, 475);
+            this.ClientSize = new System.Drawing.Size(981, 392);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Assembly certificate checker tool";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- CertificateCollectorForm/CertificateCollectorForm/Form1.cs
+++ CertificateCollectorForm/CertificateCollectorForm/Form1.cs
@@ -1,65 +1,121 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
 
 namespace CertificateCollectorForm
 {
-    using System.Globalization;
-    using System.IO;
-    using System.Reflection;
-    using System.Security.Cryptography.X509Certificates;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Threading;
 
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            this.listBox1.DataSource = this.assemblies;
         }
+
+        private List<string> assemblies = new List<string>();
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.listBox1.Items.Clear();
-            DirectoryInfo d = new DirectoryInfo(this.textBox1.Text);
+            this.progressBar1.Style = ProgressBarStyle.Marquee;
+            this.progressBar1.MarqueeAnimationSpeed = 100;
+            this.button1.Enabled = false;
+            this.assemblies.Clear();
+            this.listBox1.DataSource = null;
 
-            var list = new Dictionary<string,string>();
-            foreach (FileInfo file in d.GetFiles("*.dll"))
+            BackgroundWorker backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += DoWork;
+            backgroundWorker.RunWorkerCompleted += RunWorkerCompleted;
+            backgroundWorker.RunWorkerAsync();
+        }
+
+
+        void DoWork(object sender, DoWorkEventArgs e)
+        {
+            var dirs = Directory.GetDirectories(this.textBox1.Text, "*", SearchOption.AllDirectories).ToList();
+            dirs.Add(this.textBox1.Text);
+            foreach (var dir in dirs)
             {
-                Assembly asm = Assembly.LoadFile(file.FullName);
-                Module m = asm.GetModules()[0];
-                var cert = m.GetSignerCertificate();
-                if (cert != null)
+                DirectoryInfo directoryInfo = new DirectoryInfo(dir);
+                foreach (FileInfo file in directoryInfo.GetFiles("*.dll"))
                 {
-                    var cert2 = new X509Certificate2(cert);
-                    if (cert2.NotAfter <= DateTime.Now)
+                    int index = file.FullName.IndexOf(this.textBox1.Text);
+                    string simplifiedFileName = (index < 0)
+                                           ? "." + file.FullName
+                                           : "." + file.FullName.Remove(index, this.textBox1.Text.Length);
+                    Assembly assembly;
+                    try
                     {
-                        list.Add(file.Name, "Lejárt: "+cert.GetExpirationDateString());
+                        assembly = Assembly.LoadFile(file.FullName);
+                    }
+                    catch
+                    {
+                        this.assemblies.Add(simplifiedFileName + ": The file is blocked.");
+                        continue;
+                    }
+
+                    Module module = assembly.GetModules()[0];
+                    var certificate = module.GetSignerCertificate();
+                    if (certificate == null)
+                    {
+                        this.assemblies.Add(simplifiedFileName + ": No signature found.");
+                    }
+                    else
+                    {
+                        //var cert2 = new X509Certificate2(cert);
+                        //if (cert2.NotAfter <= DateTime.Now)
+                        //{
+                        //    list.Add(file.Name, "Lejárt: "+cert.GetExpirationDateString());
+                        //}
                     }
                 }
-                else
-                {
-                    list.Add(file.Name, "NINCS ALÁÍRVA");
-                }
-
             }
+        }
 
-            foreach (var key in list)
+        void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.progressBar1.MarqueeAnimationSpeed = 0;
+            this.progressBar1.Style = ProgressBarStyle.Blocks;
+            this.progressBar1.Value = progressBar1.Minimum;
+
+            this.button1.Enabled = true;
+            this.listBox1.DataSource = null;
+            this.listBox1.DataSource = this.assemblies;
+            MessageBox.Show("Folder analyzed successfully.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(this.textBox2.Text);
+            foreach (var item in listBox1.Items)
             {
-                this.listBox1.Items.Add(key);
+                SaveFile.WriteLine(item.ToString());
+            }
+            SaveFile.Close();
+
+            MessageBox.Show("Exported successfully to: "+ this.textBox2.Text);
+        }
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = this.folderBrowserDialog1.SelectedPath;
             }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
-        }
-
-        private void textBox1_Click(object sender, EventArgs e)
-        {
+            if (this.saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = this.saveFileDialog1.FileName;
+            }
         }
     }
 }
